b54558110bbb9617845536071834b84825e15224
AMARNATHAN-VEL <amarnathanvel@gmail.com>
2026-02-18 19:37:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import "./App.css";
+import { Counter } from "./components/Counter";
+
+function App() {
+
+  return (
+    <>
+      {/* <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1> */}
+      
+      <Counter />
+    </>
+  );
+}
+
+export default App;
